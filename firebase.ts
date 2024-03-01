2426203ebb067744f7e104afbf1d20366c4b9fd2
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-03-02 00:56:02 +0500

    Firebase init

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGFPjSlp-QjJCamgfJc-x3T27_IQIpSPY',
+  authDomain: 'alcoblock-22b15.firebaseapp.com',
+  databaseURL: 'https://alcoblock-22b15-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'alcoblock-22b15',
+  storageBucket: 'alcoblock-22b15.appspot.com',
+  messagingSenderId: '421894008292',
+  appId: '1:421894008292:web:f440aa4dcb03239f426bf1',
+  measurementId: 'G-0CGDDF0WF3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
