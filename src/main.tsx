f0df76343b8da86e9661461970e5011c943720ff
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-03-01 17:21:54 +0500

    Fix .tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client';
 import vkBridge from '@vkontakte/vk-bridge';
-import { AppConfig } from './AppConfig.tsx';
+import { createRoot } from 'react-dom/client';
+import { AppConfig } from './AppConfig';
 
 vkBridge.send('VKWebAppInit');
 
 createRoot(document.getElementById('root')!).render(<AppConfig />);
 
 if (import.meta.env.MODE === 'development') {
-  import('./eruda.ts');
+  import('./eruda');
 }
