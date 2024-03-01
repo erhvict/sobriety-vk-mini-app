5d2de4ed34c41c8483d7825b30393f9428153f06
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-03-02 01:13:11 +0500

    Some functions are commented out

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import bridge, { UserInfo } from '@vkontakte/vk-bridge';
 import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
-import { ScreenSpinner, SplitCol, SplitLayout, View } from '@vkontakte/vkui';
-import { ReactNode, useEffect, useState } from 'react';
+import { SplitCol, SplitLayout, View } from '@vkontakte/vkui';
 import { Modals } from './modals';
 
 import { Home } from './pages';
@@ -9,23 +7,23 @@ import { DEFAULT_VIEW_PANELS } from './routes';
 
 export const App = () => {
   const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
-  const [fetchedUser, setUser] = useState<UserInfo | undefined>();
-  const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
+  // const [fetchedUser, setUser] = useState<UserInfo | undefined>();
+  // const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
 
-  useEffect(() => {
-    async function fetchData() {
-      const user = await bridge.send('VKWebAppGetUserInfo');
-      setUser(user);
-      setPopout(null);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const user = await bridge.send('VKWebAppGetUserInfo');
+  //     setUser(user);
+  //     setPopout(null);
+  //   }
+  //   fetchData();
+  // }, []);
 
   return (
-    <SplitLayout popout={<Modals />}>
+    <SplitLayout modal={<Modals />}>
       <SplitCol>
         <View activePanel={activePanel}>
-          <Home id="home" fetchedUser={fetchedUser} />
+          <Home id="home" />
         </View>
       </SplitCol>
     </SplitLayout>
