230ef9b77207399ce9a596003dc63191f4424890
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-03-02 20:13:42 +0500

    Refactor AddDateModal

--- src/modals/AddDateModal.tsx
+++ src/modals/AddDateModal.tsx
@@ -1,33 +1,97 @@
+import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 import {
-  Calendar,
+  Button,
+  DateInput,
   Div,
   FormItem,
   FormLayoutGroup,
+  Input,
   ModalCard,
   ModalPageHeader,
   NavIdProps,
+  Select,
 } from '@vkontakte/vkui';
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 
 const AddDateModal: FC<NavIdProps & { onClose: () => void }> = (props) => {
+  const routeNavigator = useRouteNavigator();
   const [value, setValue] = useState(() => new Date());
+  const [alchoType, setAlchoType] = useState('');
+  const [volume, setVolume] = useState('');
+
+  const onConfirmClick = useCallback(() => {
+    routeNavigator.back();
+  }, [routeNavigator]);
+
+  // console.log(`Дата: ${value}. Алкоголь: ${alchoType}. Количество: ${volume}`);
 
   return (
     <ModalCard {...props} header={<ModalPageHeader>Добавить выпитое</ModalPageHeader>}>
       <Div>
         <FormLayoutGroup mode="vertical">
-          <FormItem>
-            <Calendar
+          <FormItem required top="Дата">
+            <DateInput
               value={value}
               onChange={(newValue) => setValue(newValue as Date)}
-              enableTime={true}
+              enableTime={false}
               disablePast={false}
               disableFuture={true}
               disablePickers={true}
               showNeighboringMonth={true}
               size="m"
-              listenDayChangesForUpdate={true}
             />
+          </FormItem>
+          <FormItem required top="Напиток">
+            <Select
+              placeholder="Выберите напиток"
+              value={alchoType}
+              onChange={(e) => setAlchoType(e.target.value)}
+              options={[
+                {
+                  label: 'Пиво',
+                  value: 'beer',
+                },
+                {
+                  label: 'Крепкое',
+                  value: 'strong ',
+                },
+                {
+                  label: 'Сидр',
+                  value: 'cider',
+                },
+                {
+                  label: 'Вино',
+                  value: 'wine',
+                },
+
+                {
+                  label: 'Шампанское',
+                  value: 'champagne',
+                },
+                {
+                  label: 'Коктейль',
+                  value: 'cocktail',
+                },
+                {
+                  label: 'Ликер',
+                  value: 'liquor',
+                },
+                {
+                  label: 'Другое',
+                  value: 'other',
+                },
+              ]}
+            />
+          </FormItem>
+
+          {!alchoType ? null : (
+            <FormItem required top="Количество выпитого, л">
+              <Input placeholder="0.5" value={volume} onChange={(e) => setVolume(e.target.value)} />
+            </FormItem>
+          )}
+
+          <FormItem>
+            <Button onClick={onConfirmClick}>Добавить</Button>
           </FormItem>
         </FormLayoutGroup>
       </Div>
