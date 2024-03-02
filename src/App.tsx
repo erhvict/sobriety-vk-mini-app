49c0e26cd9ca63c98322a4db36f2608d03d1336b
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-03-02 20:13:05 +0500

    Add ads init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import bridge, { BannerAdLayoutType, BannerAdLocation } from '@vkontakte/vk-bridge';
 import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
 import { SplitCol, SplitLayout, View } from '@vkontakte/vkui';
 import { Modals } from './modals';
 
 import { Home } from './pages';
 import { DEFAULT_VIEW_PANELS } from './routes';
+
+bridge
+  .send('VKWebAppShowBannerAd', {
+    banner_location: BannerAdLocation.BOTTOM,
+    layout_type: BannerAdLayoutType.RESIZE,
+  })
+  .then((data) => {
+    if (data.result) {
+      console.log('Реклама появилась');
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 export const App = () => {
   const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
