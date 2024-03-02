197d98c01ef8c6bc2b7d6cedb66b80d7ecc6b883
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-03-03 00:00:48 +0500

    Add fetch users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import bridge, { BannerAdLayoutType, BannerAdLocation } from '@vkontakte/vk-bridge';
+import bridge, { BannerAdLayoutType, BannerAdLocation, UserInfo } from '@vkontakte/vk-bridge';
 import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
-import { SplitCol, SplitLayout, View } from '@vkontakte/vkui';
+import { ScreenSpinner, SplitCol, SplitLayout, View } from '@vkontakte/vkui';
+import { ReactNode, useEffect, useState } from 'react';
 import { Modals } from './modals';
 
 import { Home } from './pages';
@@ -11,34 +12,29 @@ bridge
     banner_location: BannerAdLocation.BOTTOM,
     layout_type: BannerAdLayoutType.RESIZE,
   })
-  .then((data) => {
-    if (data.result) {
-      console.log('Реклама появилась');
-    }
-  })
   .catch((error) => {
     console.log(error);
   });
 
 export const App = () => {
+  const [fetchedUser, setUser] = useState<UserInfo | undefined>();
   const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
-  // const [fetchedUser, setUser] = useState<UserInfo | undefined>();
-  // const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
+  const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const user = await bridge.send('VKWebAppGetUserInfo');
-  //     setUser(user);
-  //     setPopout(null);
-  //   }
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const user = await bridge.send('VKWebAppGetUserInfo');
+      setUser(user);
+      setPopout(null);
+    }
+    fetchData();
+  }, []);
 
   return (
     <SplitLayout modal={<Modals />}>
       <SplitCol>
         <View activePanel={activePanel}>
-          <Home id="home" />
+          <Home id="home" fetchedUser={fetchedUser} />
         </View>
       </SplitCol>
     </SplitLayout>
